182f8d08b3fcf3597fa55e4da9dea5cde4ab1a24
michailid <michailidoumaria224@gmail.com>
2026-09-24 18:22:06 +0300

    login/logout functionality & navbar links

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { LOGGED_USER_KEY } from '../../constants/constants';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   selector: 'app-layout',
   styleUrl: './layout.css',
   templateUrl: './layout.html',
 })
-export class Layout {}
+export class Layout {
+  loggedInUser = signal(JSON.parse(localStorage.getItem(LOGGED_USER_KEY)!));
+
+  onLogout() {
+    localStorage.removeItem(LOGGED_USER_KEY);
+  }
+}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { LOGGED_USER_KEY } from '../../constants/constants';
+import { Router } from '@angular/router';
 
 @Component({
   imports: [FormsModule],
@@ -10,12 +13,36 @@ import { FormsModule } from '@angular/forms';
 export class Login {
   email: string = '';
   mobileNo: string = '';
+  logggedInUser = {};
+  customersData: any[] = [];
+
+  http = inject(HttpClient);
+  router = inject(Router);
 
   onLogin() {
-    if (this.email === 'asdf@gmail.com' && this.mobileNo === '1234567890') {
+    this.getAllCustomers();
+    this.logggedInUser = this.customersData.find(
+      (c) => c.email == this.email && c.mobileNo == this.mobileNo,
+    );
+
+    if (this.logggedInUser) {
+      localStorage.setItem(LOGGED_USER_KEY, JSON.stringify(this.logggedInUser));
       console.log('Logged in successfully');
+      this.router.navigateByUrl('dashboard');
     } else {
       console.log('Wrong credentials');
     }
+  }
+
+  getAllCustomers() {
+    this.http.get('https://freeapi.gerasim.in/api/CarRentalApp/GetCustomers').subscribe({
+      next: (response: any) => {
+        this.customersData = response.data;
+        console.log(response);
+      },
+      error: (error) => {
+        alert('Error: ' + error);
+      },
+    });
   }
 }
